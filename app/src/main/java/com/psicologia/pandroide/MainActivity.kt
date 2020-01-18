5de12c079e2b96952d67165dfc1bf584cda07977
u201614342@upc.edu.pe <u201614342@upc.edu.pe>
2020-01-18 01:20:20 -0500

    login y registro

--- app/src/main/java/com/psicologia/pandroide/MainActivity.kt
+++ app/src/main/java/com/psicologia/pandroide/MainActivity.kt
@@ -2,11 +2,41 @@ package com.psicologia.pandroide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showHome()
+
+        registration.setOnClickListener {
+            showRegistration()
+        }
+        login.setOnClickListener {
+            showLogIN()
+        }
+    }
+
+
+    private fun showRegistration(){
+        registration_layout.visibility= View.VISIBLE
+        login_layout.visibility=View.GONE
+        home_ll.visibility=View.GONE
+    }
+
+    private fun showLogIN(){
+        registration_layout.visibility=View.GONE
+        login_layout.visibility=View.VISIBLE
+        home_ll.visibility=View.GONE
+    }
+    private fun showHome(){
+        registration_layout.visibility=View.GONE
+        login_layout.visibility=View.GONE
+        home_ll.visibility=View.VISIBLE
+
     }
 }
