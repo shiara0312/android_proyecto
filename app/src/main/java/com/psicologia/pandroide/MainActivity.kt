6750f239abcfa1904f8cb0c9350dfe33f2111ec2
alexqv2 <alex-92quintana@hotmail.com>
2020-02-28 00:58:08 -0500

    auth

--- app/src/main/java/com/psicologia/pandroide/MainActivity.kt
+++ app/src/main/java/com/psicologia/pandroide/MainActivity.kt
@@ -3,9 +3,14 @@ package com.psicologia.pandroide
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +34,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showLogIN(){
+        auth = FirebaseAuth.getInstance()
         registration_layout.visibility=View.GONE
         login_layout.visibility=View.VISIBLE
         home_ll.visibility=View.GONE
